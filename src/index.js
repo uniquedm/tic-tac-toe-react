78e523397aa31dd0a93079d4d803ace66c828f21
Devansh Mishra <devanshunique@gmail.com>
2022-12-25 16:40:11 +0530

    Basic List Component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class ShoppingList extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1>Shopping List for {this.props.x}</h1>
+          <ul class="list-group">
+            <li class="list-group-item">Instagram</li>
+            <li class="list-group-item">WhatsApp</li>
+            <li class="list-group-item">Oculus</li>
+          </ul>
+        </div>
+      );
+    }
+  }
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<ShoppingList x="Mark" />);
